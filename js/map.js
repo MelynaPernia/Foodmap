5162130c9754a3663a1f23e2c96af6b2632ddecc
MelynaPernia <mebipeos@outlook.com>
2017-12-26 13:30:10 -0500

    Diseñando el index, la vista main y añadiendo el mapa

---
+++ js/map.js
@@ -0,0 +1,20 @@
+
+function myMap() {
+var latitud;
+var longitud;
+
+navigator.geolocation.getCurrentPosition(function(localizacion){
+  latitud = localizacion.coords.latitude;
+  longitud = localizacion.coords.longitude;
+  console.log(latitud);
+// console.log(localizacion.coords.latitude);
+// console.log(localizacion.coords.longitude);
+var mapProp= {
+    center:new google.maps.LatLng(latitud,longitud),
+    zoom:14,
+}
+var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+})
+}
+// console.log(latitud);
