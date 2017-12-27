e6008915a72d3b78c5bac090fcf11c957fd70bf9
MelynaPernia <mebipeos@outlook.com>
2017-12-26 21:55:21 -0500

    añadiendo la data y modificando el diseño de las vistas

---
+++ js/data.js
@@ -0,0 +1,57 @@
+var data = {
+  'food-criolla': [
+    {
+      'name': 'Ají de gallina',
+      'precio': 15.50,
+      'photo': 'aji-gallina.jpg',
+      'active': true
+    },
+    {
+      'name': 'Arroz con pollo',
+      'precio': 12.00,
+      'photo': 'arroz-pollo.jpg',
+      'active': true
+    },
+    {
+      'name': 'Cau cau',
+      'precio': 15.50,
+      'photo': 'cau-cau.jpg',
+      'active': true
+    }
+  ],
+  'food-marina': [
+    {
+      'name': 'Arroz con mariscos',
+      'precio': 25.90,
+      'photo': 'arroz-mariscos.jpg',
+      'active': true
+    },
+    {
+      'name': 'Ceviche',
+      'precio': 28.00,
+      'photo': 'ceviche.jpg',
+      'active': true
+    },
+    {
+      'name': 'Jalea',
+      'precio': 29.90,
+      'photo': 'jalea.jpg',
+      'active': true
+    }
+  ],
+  'food-selva': [
+    {
+      'name': 'Juanes',
+      'precio': 15.90,
+      'photo': 'juanes.jpg',
+      'active': true
+    },
+    {
+      'name': 'Tacacho',
+      'precio': 17.90,
+      'photo': 'tacacho.jpg',
+      'active': true
+    }
+  ]
+
+};
